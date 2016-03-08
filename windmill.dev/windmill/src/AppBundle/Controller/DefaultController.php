7fc96ed0605210b784ed72e3dd4b4d03e2a8ae79
Benoit Deglane <deglane.benoit@gmail.com>
2016-03-08 13:48:41 +0100

    clean

--- windmill.dev/windmill/src/AppBundle/Controller/DefaultController.php
+++ windmill.dev/windmill/src/AppBundle/Controller/DefaultController.php
@@ -6,8 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('AppBundle:Default:index.html.twig');
-    }
+	public function indexAction()
+	{
+		$items = [
+			[
+				"url" => "/",
+				"name" => "test"
+			],
+			[
+				"url" => "/",
+				"name" => "test-2"
+			]
+		];
+
+		return $this->render('AppBundle:Default:Container/container.html.twig', ["items" => $items]);
+	}
 }
